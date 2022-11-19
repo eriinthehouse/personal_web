fb2df624b5ef53d6adc5a894ab0715af33692b70
eri <erblintopalli2005@gmail.com>
2022-11-19 18:54:18 +0100

    my comment of what I just did

--- try1.js
+++ try1.js
@@ -1,29 +1,3 @@
-function myFunction( hide, show ) {
-    document.getElementById(div3).style.display="none";
-    document.getElementById(div2).style.display="none";
-    document.getElementById(div1).style.display="none";
-    document.getElementById(div).style.display="block";
-  }
 
-  function myFunction1( hide, show ) {
-    document.getElementById(div3).style.display="none";
-    document.getElementById(div2).style.display="none";
-    document.getElementById(div).style.display="none";
-    document.getElementById(div1).style.display="block";
-  }
-
-  function myFunction2( hide, show ) {
-    document.getElementById(div3).style.display="none";
-    document.getElementById(div).style.display="none";
-    document.getElementById(div1).style.display="none";
-    document.getElementById(div2).style.display="block";
-  }
-
-  function myFunction3( hide, show ) {
-    document.getElementById(div).style.display="none";
-    document.getElementById(div2).style.display="none";
-    document.getElementById(div1).style.display="none";
-    document.getElementById(div3).style.display="block";
-  }
 
 
